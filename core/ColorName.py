76ed785be5e3fd2569411392eff8b604395b2511
Sylvain DEKER <sldkr@gmx.com>
2020-02-06 17:20:43 +0100

    Add Learning KMeans

--- core/ColorName.py
+++ core/ColorName.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from ImageVehicle import ImageVehicle
+from buildSetImageVehicle import buildSetImageVehicle
 import cv2 as cv
 from sklearn.cluster import KMeans
 
@@ -13,12 +14,13 @@ class ColorName:
 
 
     def extract_patchs(self,image_vehicule:ImageVehicle):
-        desc = np.empty((16,3))
-        # img2 = image_vehicule.image
-        # cv.imwrite("test.png",img2)
-        img2 = image_vehicule.image[0:16:4,4:32:8]
-        # cv.imwrite("test2.png",img2)
-        desc = np.reshape(img2,(16,3))
+        img = cv.imread(image_vehicule.image)
+        edge = 4
+        img2 = img[edge:img.shape[0]-edge:1,edge:img.shape[1]-edge:1]
+        lenPatch = img2.shape[0]*img2.shape[1]
+        desc = np.empty((lenPatch,3))
+
+        desc = np.reshape(img2,(lenPatch,3))
         return desc
 
 
@@ -70,6 +72,17 @@ class ColorName:
         moyenne = np.mean(Xres,axis=0)
         return moyenne
 
+    def trainColorName(self,ArrayOfImageVehicle):
+        NbImages = len(ArrayOfImageVehicle)
+        colors = np.empty((NbImages,3))
+        for i in range(NbImages):
+            desc = self.extract_patchs(ArrayOfImageVehicle[i])
+            self.fitKMeans(desc,4)
+            Xres,label_dominant = cn.bow_process(desc)
+            cv.imwrite("test_Xres.png",np.resize(Xres,(len(Xres),1,3)))
+            colors[i] = cn.resultColor(Xres)
+        return colors
+
 
 
 
@@ -78,33 +91,44 @@ if __name__ == '__main__':
     # img = cv.imread("../data/VeRi_with_plate/image_query/0300_c013_00078770_0.jpg")
     # img = cv.imread("../data/VeRi_with_plate/image_query/0002_c002_00030600_0.jpg")
     # img = cv.imread("../data/VeRi_with_plate/image_query/0063_c016_00007580_0.jpg")
-    img = cv.imread("../data/VeRi_with_plate/image_query/0172_c011_00078830_0.jpg")
+    # img = cv.imread("../data/VeRi_with_plate/image_query/0172_c011_00078830_0.jpg")
 
+    #
+    # img = cv.resize(img,(32,32))
+    # cv.imwrite("test_ref.png",img)
+    # iv = ImageVehicle(img,"/chemin/video",((0,0,0,0,0,0)),42)
+    # cn = ColorName()
+    # desc = cn.extract_patchs(iv)
+    # cv.imwrite("test_desc.png",np.resize(desc,(desc.shape[0],1,3)))
+    #
+    # # print(desc)
+    # print("----------KMMEANS Test-----------")
+    # # Example of clusters
+    # # rouge = [255,0,0]
+    # # jaune = [255,255,0]
+    # # vert = [0,255,0]
+    # # bleu = [0,0,255]
+    # #
+    # # white = [255,255,255]
+    # # gray = [128,128,128]
+    # # black = [0,0,0]
+    # # X = np.array([rouge,jaune,vert,bleu,gray,white,black])
+    #
+    # cn.fitKMeans(desc,4)
+    # Xres,label_dominant = cn.bow_process(desc)
+    # cv.imwrite("test_Xres.png",np.resize(Xres,(len(Xres),1,3)))
+    # result_color = cn.resultColor(Xres)
+    # cv.imwrite("test_ResColor.png",np.resize(result_color,(1,1,3)))
+    #
+    # score = cn.score(result_color,label_dominant)
+    # print("Score")
+    # print(score)
 
-    img = cv.resize(img,(32,16))
-    cv.imwrite("test_ref.png",img)
-    iv = ImageVehicle(img,"/chemin/video",((0,0,0,0,0,0)),42)
+    print("--------trainColor--------------")
     cn = ColorName()
-    desc = cn.extract_patchs(iv)
-    cv.imwrite("test_desc.png",np.resize(desc,(16,1,3)))
-
-    # print(desc)
-    print("----------KMMEANS Test-----------")
-    # Example of clusters
-    rouge = [255,0,0]
-    jaune = [255,255,0]
-    jaune2 = [255,255,10]
-    vert = [0,255,0]
-    bleu = [0,0,255]
-    white = [255,255,255]
-    X = np.array([rouge,vert,jaune2,bleu,jaune,white])
-
-    cn.fitKMeans(X,4)
-    Xres,label_dominant = cn.bow_process(desc)
-    cv.imwrite("test_Xres.png",np.resize(Xres,(len(Xres),1,3)))
-    result_color = cn.resultColor(Xres)
-    cv.imwrite("test_ResColor.png",np.resize(result_color,(1,1,3)))
-
-    score = cn.score(result_color,label_dominant)
-    print("Score")
-    print(score)
+    x=300
+    y=100
+    arrayOfImageVehicle = buildSetImageVehicle("../data/VeRi_with_plate/image_train/",x*y)
+    colors = cn.trainColorName(arrayOfImageVehicle)
+    cv.imwrite("colors.png",np.resize(colors,(x,y,3)))
+    np.savetxt('colors.csv', colors, fmt='%i')
