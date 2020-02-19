925700bcb5608eb9f3030b82f8f5e13bae068dd1
unknown <nahor@hotmail.fr>
2020-02-19 18:01:33 +0100

    Typo

--- core/googleNet.py
+++ core/googleNet.py
@@ -1,20 +1,20 @@
-def getSementic(imageName):
+def getSemantic(imageName):
   return(np.loadtxt(imageName))
 
-def saveSementic(imageName,features):
+def saveSemantic(imageName,features):
   np.savetxt(imageName,features)
 
-def prepareSementicDataSet(featureExtractor):
+def prepareSemanticDataSet(featureExtractor):
   for file in os.listdir("./vehicule/"):
     img = image.load_img(file, target_size=(IMG_HEIGHT, IMG_WIDTH))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     features = featureExtractor.predict(x)
-    saveSementic('./sementicFolder/'+file,features)
-	
+    saveSemantic('./semanticFolder/'+file,features)
+
 def googleNetScore(img_path,featureExtractor):
-  sementicScore = []
+  semanticScore = []
   name = []
   img = image.load_img(img_path, target_size=(IMG_HEIGHT, IMG_WIDTH))
   x = image.img_to_array(img)
@@ -22,12 +22,12 @@ def googleNetScore(img_path,featureExtractor):
   x = preprocess_input(x)
   features = featureExtractor.predict(x)
 
-  for file in os.listdir("./sementicFolder/"):
-    sementicScore = sementicScore.append(distance.euclidean(features,getSementic(file)))
+  for file in os.listdir("./semanticFolder/"):
+    semanticScore = semanticScore.append(distance.euclidean(features,getSemantic(file)))
     name = name.append(file)
-  listeTrie = list(zip(sementicScore, name))
+  listeTrie = list(zip(semanticScore, name))
   newListe = sorted(listeTrie, key=lambda score: score[0])
   return(newListe)
-  
+
 featureExtractor = load_model("./featureExtractor_model.hdf5")
 featureExtractor.load_weights("./featureExtractor_weight.hdf5")
